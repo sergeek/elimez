74cd97c6f9b8ea0214168ef806c6e5092c8a0763
Sergey Polishchuk <sergeek@live.com>
2017-10-24 21:39:01 -0700

    Configured for Heroku

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://elimez:unitfour@localhost:8889/elimez'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://elimez:unitfour@localhost:8889/elimez')
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
